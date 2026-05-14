9d9998dfc34341fc7c2ece5a2d31609acd56922d
Abdelrhman <149049648+AbdelrhmanRafia@users.noreply.github.com>
2026-05-14 18:04:53 +0200

    rem

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bookingRoute = require("./routes/bookingRoute");
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const path = require("path");
-const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -27,10 +26,7 @@ app.use(
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
-// Development logging
-if (process.env.NODE_ENV === "development") {
-}
-app.use(morgan("dev"));
+
 
 app.set("query parser", "extended");
 
